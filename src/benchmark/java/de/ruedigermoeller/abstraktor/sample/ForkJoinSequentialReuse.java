e694d5bcc6527342902c6a8c9d59544d691995fb
RuedigerMoeller <moru0011@gmail.com>
2014-04-17 20:12:44 +0200

    Update ForkJoinSequentialReuse.java

--- src/benchmark/java/de/ruedigermoeller/abstraktor/sample/ForkJoinSequentialReuse.java
+++ src/benchmark/java/de/ruedigermoeller/abstraktor/sample/ForkJoinSequentialReuse.java
@@ -7,26 +7,8 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.LockSupport;
 
 /**
- * Copyright (c) 2012, Ruediger Moeller. All rights reserved.
- * <p/>
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * <p/>
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * <p/>
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
- * <p/>
- * Date: 04.01.14
- * Time: 02:54
- * To change this template use File | Settings | File Templates.
+ * my shot on processing sequential independent jobs with FJ. Better single thread
+ * performance, worse scaling ..
  */
 public class ForkJoinSequentialReuse {
 
