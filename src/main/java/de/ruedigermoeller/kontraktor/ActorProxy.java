94b275ab98d1e100d2f3171f4462fe61c54ba7c9
RuedigerMoeller <moru0011@gmail.com>
2014-05-11 19:35:12 +0200

    fix javadoc errors

--- src/main/java/de/ruedigermoeller/kontraktor/ActorProxy.java
+++ src/main/java/de/ruedigermoeller/kontraktor/ActorProxy.java
@@ -2,22 +2,22 @@ package de.ruedigermoeller.kontraktor;
 
 /**
 * Copyright (c) 2012, Ruediger Moeller. All rights reserved.
-* <p/>
+*
 * This library is free software; you can redistribute it and/or
 * modify it under the terms of the GNU Lesser General Public
 * License as published by the Free Software Foundation; either
 * version 2.1 of the License, or (at your option) any later version.
-* <p/>
+*
 * This library is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 * Lesser General Public License for more details.
-* <p/>
+*
 * You should have received a copy of the GNU Lesser General Public
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 * MA 02110-1301  USA
-* <p/>
+*
 * Date: 03.01.14
 * Time: 21:00
 * To change this template use File | Settings | File Templates.
@@ -35,7 +35,6 @@ public interface ActorProxy<T extends Actor> {
     /**
      * for internal use only
      * @param disp
-     * @return
      */
     public void __setDispatcher(DispatcherThread disp);
 
--- src/main/java/de/ruedigermoeller/kontraktor/Actors.java
+++ src/main/java/de/ruedigermoeller/kontraktor/Actors.java
@@ -9,22 +9,22 @@ import java.util.concurrent.*;
 
 /**
  * Copyright (c) 2012, Ruediger Moeller. All rights reserved.
- * <p/>
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * <p/>
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * <p/>
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301  USA
- * <p/>
+ *
  * Date: 04.01.14
  * Time: 19:50
  * To change this template use File | Settings | File Templates.
@@ -181,10 +181,8 @@ public class Actors {
      * return a new dispatcher backed by a new thread. Overriding classes should *not*
      * return existing dispatchers here, as this can be used to isolate blocking code from the actor flow.
      *
-     * if qSiz <= 0 use default size
+     * if qSiz lesser or equal 0 use default size
      * @return
-     * @throws InstantiationException
-     * @throws IllegalAccessException
      */
     protected DispatcherThread newDispatcher(int qSize) {
         return new DispatcherThread(qSize);
