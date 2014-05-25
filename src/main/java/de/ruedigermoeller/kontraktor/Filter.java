22ddd7f0594ea5315dba3c8ce0834d2ce37649b4
RuedigerMoeller <moru0011@gmail.com>
2014-05-25 02:20:52 +0200

    Update Filter.java

--- src/main/java/de/ruedigermoeller/kontraktor/Filter.java
+++ src/main/java/de/ruedigermoeller/kontraktor/Filter.java
@@ -2,6 +2,8 @@ package de.ruedigermoeller.kontraktor;
 
 /**
  * Created by ruedi on 23.05.14.
+ * 
+ * works like 'then' but passes the future returned to the next filter() or then() call in a future chain.
  */
 public interface Filter<IN,OUT> {
 
