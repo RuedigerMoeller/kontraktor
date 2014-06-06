ec4116aee25bae8b899df111aa9bac5385d8aea8
RuedigerMoeller <moru0011@gmail.com>
2014-06-06 10:20:16 +0200

    channels ongoing

--- src/main/java/de/ruedigermoeller/kontraktor/Channel.java
+++ src/main/java/de/ruedigermoeller/kontraktor/Channel.java
@@ -50,85 +50,43 @@ public class Channel<T> implements Future<T> {
     }
 
     @Override
+    public T getResult() {
+        return (T) result.peek(); // fixme: wrong
+    }
+
+    @Override
     public Future then(Callback resultCB) {
         // FIXME: this can be implemented more efficient
-        while( !lock.compareAndSet(false,true) ) {}
         resultReceiver = resultCB;
-        if (hadResult) {
-            if ( hasFired ) {
-                lock.set(false);
-                throw new RuntimeException("Double result received on future");
-            }
-            hasFired = true;
-            lock.set(false);
-            nextFuture = new Promise(result,error);
-            resultCB.receiveResult( result, error );
-            return nextFuture;
-        } else {
-            lock.set(false);
-        }
         if ( resultCB instanceof Future) {
             return (Future)resultCB;
         }
         return nextFuture = new Promise();
     }
 
-    @Override
-    public final void receiveResult(Object res, Object error) {
-        // ensure correct thread in case actor cascades futures
-        if ( Thread.currentThread() != currentThread && currentThread instanceof DispatcherThread) {
-            new CallbackWrapper((DispatcherThread) currentThread, this).receiveResult(res, error);
-        }
-        else {
-            this.result = res;
-            this.error = error;
-            while( !lock.compareAndSet(false,true) ) {}
-            if ( hadResult ) {
-                lock.set(false);
-                throw new RuntimeException("Double result received on future");
-            }
-            hadResult = true;
-            if ( resultReceiver != null ) {
-                if ( hasFired ) {
-                    lock.set(false);
-                    throw new RuntimeException("Double fire on callback");
-                }
-                hasFired = true;
-                lock.set(false);
-                resultReceiver.receiveResult(result,error);
-                resultReceiver = null;
-                if ( nextFuture != null )
-                    nextFuture.receiveResult(result,error);
-                return;
-            } else {
-                lock.set(false);
-            }
+    private void poll() {
+        if ( resultReceiver != null )
+            return;
+        Object poll = null;
+        while ((poll=result.poll())!=null) {
+            resultReceiver.receiveResult(poll, null);
         }
     }
 
-    public T getResult() {
-        return (T) result;
+    @Override
+    public final void receiveResult(Object res, Object error) {
+        result.offer(res);
+        poll();
     }
 
     public Object getError() {
-        return error;
-    }
-
-    // debug
-    public boolean _isHadResult() {
-        return hadResult;
-    }
-
-    // debug
-    public boolean _isHasFired() {
-        return hasFired;
+        return null;
     }
 
     @Override
     public String toString() {
         return "Result{" +
-                "result=" + result +
-                ", error=" + error +
+                "result=" + result.size() +
                 '}';
     }
 
