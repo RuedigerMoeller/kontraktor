2700298ec02f7eed19103e61c34baa0a70833ad5
RuedigerMoeller <moru0011@gmail.com>
2014-09-25 17:52:42 +0200

    .

--- src/test/java/kontraktor/scheduling/Dining.java
+++ src/test/java/kontraktor/scheduling/Dining.java
@@ -4,7 +4,7 @@ import org.nustaq.kontraktor.Actor;
 import org.nustaq.kontraktor.Actors;
 import org.nustaq.kontraktor.Future;
 import org.nustaq.kontraktor.Promise;
-import org.nustaq.kontraktor.annotations.CallerSideMethod;
+import org.nustaq.kontraktor.impl.ElasticScheduler;
 import org.nustaq.kontraktor.util.Hoarde;
 
 import java.util.ArrayList;
@@ -15,79 +15,90 @@ import java.util.concurrent.locks.LockSupport;
  */
 public class Dining {
 
-    public static class TableCoordinator extends Actor<TableCoordinator> {
+    public static class Coordinator extends Actor<Coordinator> {
         ArrayList<Promise> forks[] = new ArrayList[5];
 
-        public TableCoordinator() {
-            for (int i = 0; i < forks.length; i++) {
+        public Coordinator() {
+            for (int i = 0; i < forks.length; i++)
                 forks[i] = new ArrayList<>();
-            }
         }
 
         public Future getFork( int num ) {
-            num = num % 5;
-            Promise res;
-            if ( forks[num].size() == 0 ) {
-                res = new Promise("void");
-            } else {
-                res = new Promise();
-            }
+            num %= 5;
+            Promise res = forks[num].size() == 0 ? new Promise("void") : new Promise();
             forks[num].add(res);
             return res;
         }
 
         public void returnFork(int num) {
-            num = num % 5;
+            num %= 5;
             forks[num].remove(0);
             if ( forks[num].size() > 0 )
-                forks[num].get(0).receive("void",null);
+                forks[num].get(0).signal();
         }
     }
 
     public static class Philosopher extends Actor<Philosopher> {
         String name;
         int nr;
-        TableCoordinator tableCoordinator;
-        volatile String state;
+        Coordinator coordinator;
+        String state;
+        int eatCount;
 
-        public void $start(String name, int nr, TableCoordinator tableCoordinator) {
+        public void $start(String name, int nr, Coordinator coordinator) {
             this.name = name;
             this.nr = nr;
-            this.tableCoordinator = tableCoordinator;
+            this.coordinator = coordinator;
             $think();
         }
 
         public void $think() {
-            long thinkTime = (long) (5000 * Math.random());
+            long thinkTime = (long) (100 * Math.random());
             state = "Think";
+            int firstFork, secondFork;
+            if ( (nr & 1) == 0 ) {
+                firstFork = nr; secondFork = nr+1;
+            } else {
+                firstFork = nr+1; secondFork = nr;
+            }
             delayed(thinkTime, () -> {
                 state = "Hungry";
-                yield( tableCoordinator.getFork(nr), tableCoordinator.getFork(nr + 1) ).then((r, e) -> {
-                    state = "Eat";
-                    delayed(2000, () -> {
-                        tableCoordinator.returnFork(nr);
-                        tableCoordinator.returnFork(nr + 1);
-                        self().$think();
+                coordinator.getFork(firstFork).then((r, e) -> {
+                    coordinator.getFork(secondFork).then((r1, e1) -> {
+                        state = "Eat";
+                        long eatTime = (long) (100 * Math.random());
+                        delayed(eatTime, () -> {
+                            eatCount++;
+                            coordinator.returnFork(firstFork);
+                            coordinator.returnFork(secondFork);
+                            self().$think();
+                        });
                     });
                 });
             });
         }
 
-        @CallerSideMethod String getState() {
-            return getActor().name+" "+getActor().state;
+        public Future<String> $getState() {
+            return new Promise( name+" "+state+" eaten:"+eatCount );
         }
     }
 
     public static void main( String arg[] ) {
         String names[] = { "A", "B", "C", "D", "E" };
-        TableCoordinator tableCoordinator = Actors.AsActor(TableCoordinator.class);
-        Hoarde<Philosopher> phils = new Hoarde<>( 5, Philosopher.class );
-        phils.each( (phil, index) -> phil.$start(names[index], index, tableCoordinator));
+        Coordinator coordinator = Actors.AsActor(Coordinator.class);
+        Hoarde<Philosopher> phils = new Hoarde<>( 5, Philosopher.class, new ElasticScheduler(1));
+
+        phils.each( (phil, index) -> phil.$start(names[index], index, coordinator));
+
+        // start a thread reporting state each second
         new Thread(() -> {
             while( true ) {
                 LockSupport.parkNanos(1000 * 1000l * 1000);
-                phils.each((phil) -> System.out.print(phil.getState() + ", ") );
-                System.out.println();
+                Actors.yield( phils.map( (phil, index) -> phil.$getState() ) ).then( (futs, e) -> {
+                    for (int i = 0; i < futs.length; i++)
+                        System.out.print(futs[i].getResult() + ", ");
+                    System.out.println();
+                });
             }
         }).start();
     }
