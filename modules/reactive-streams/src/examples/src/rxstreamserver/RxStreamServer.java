be4c6b10cb59d6d76aa35e3b9b5d6df33c5f046e
moru0011 <moru0011@gmail.com>
2015-07-06 09:42:25 +0200

    Update RxStreamServer.java

--- modules/reactive-streams/src/examples/src/rxstreamserver/RxStreamServer.java
+++ modules/reactive-streams/src/examples/src/rxstreamserver/RxStreamServer.java
@@ -16,7 +16,7 @@ import java.util.stream.IntStream;
 /**
  * Created by ruedi on 05/07/15.
  *
- * By default using "RxSubscriber.serve()", one stream is published on the network on a single address (e.g. localhost:7777).
+ * With "RxSubscriber.serve()", one stream is published on the network on a single address (e.g. localhost:7777).
  * Once the stream is complete,
  * the associated network connection will terminate. Due to network related latency, it takes a while until the address
  * can be reused. Therefore this style of exposing streams is mostly suited for "infinite streams". "onComplete" then
