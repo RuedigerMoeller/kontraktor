6c7f058489da773a802f6371af5a84d76e8cab90
RuedigerMoeller <moru0011@gmail.com>
2015-09-23 18:51:22 +0200

    .

--- release.sh
+++ release.sh
@@ -13,6 +13,7 @@ cd modules/kontraktor-bare
 mvn clean package -Dmaven.test.skip=true gpg:sign
 cd target
 rm -r */
+rm *-jar-with-dep*
 jar -cf bundle.jar *
 cd ..
 mvn package -Dmaven.test.skip=true # rebuild fat jars
@@ -21,12 +22,11 @@ cd ../kontraktor-http
 
 mvn clean package -Dmaven.test.skip=true gpg:sign
 cd target
-rm *-jar-with*
 rm -r */
+rm *-jar-with*
 jar -cf bundle.jar *
 cd ..
 mvn install -Dmaven.test.skip=true # install
-
 
 cd ../reactive-streams
 
