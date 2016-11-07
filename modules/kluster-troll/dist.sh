da6426b42aa4c99e13ef82cab5d3c0a4e9058d33
RuedigerMoeller <moru0011@gmail.com>
2016-11-07 14:20:52 +0100

    restart option

--- modules/kluster-troll/dist.sh
+++ modules/kluster-troll/dist.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mvn clean package
-cp -f ./target/kluster-troll-3.24-jar-with-dependencies.jar ./dist/kkluster.jar
+cp -f ./target/kluster-troll-*-jar-with-dependencies.jar ./dist/kkluster.jar
