3200f5e4932a958c1f6b466fece35aa0ae316bda
RuedigerMoeller <moru0011@gmail.com>
2016-11-07 14:13:27 +0100

    restart

--- modules/kluster-troll/src/main/java/org/nustaq/kluster/processes/ProcessStarter.java
+++ modules/kluster-troll/src/main/java/org/nustaq/kluster/processes/ProcessStarter.java
@@ -23,6 +23,7 @@ import java.util.stream.Collectors;
  */
 public class ProcessStarter extends Actor<ProcessStarter> {
 
+    public static final int RESTART_EXIT_VAL = 99;
     // discovery time = 2 * TICK_M
     public static int TICK_MILLIS = 3_000;
 
@@ -224,14 +225,18 @@ public class ProcessStarter extends Actor<ProcessStarter> {
                 // check if processes died
                 processes = processes.entrySet().stream()
                     .filter(en -> {
-                        if (en.getValue().getProc() == null) {
-                            if (!siblings.containsKey(en.getValue().getStarterId())) // host died
+                        ProcessInfo procInfo = en.getValue();
+                        if (procInfo.getProc() == null) {
+                            if (!siblings.containsKey(procInfo.getStarterId())) // host died
                             {
                                 return false;
                             }
                         } else {
-                            if (!en.getValue().getProc().isAlive()) {
-                                self().distribute(TERMINATED, en.getValue(), null);
+                            if (!procInfo.getProc().isAlive()) {
+                                self().distribute(TERMINATED, procInfo, null);
+                                if ( procInfo.getProc().exitValue() == RESTART_EXIT_VAL) {
+                                    self().startProcessBySpec(procInfo.getSpec());
+                                }
                                 return false;
                             }
                         }
