bdb6df8445dc0fff84896d2e6fd9396a1e1c30cb
RuedigerMoeller <moru0011@gmail.com>
2017-07-03 10:27:20 +0200

    .

--- modules/kontraktor-http/src/main/java/org/nustaq/kontraktor/weblication/DefaultSessionStorage.java
+++ modules/kontraktor-http/src/main/java/org/nustaq/kontraktor/weblication/DefaultSessionStorage.java
@@ -5,8 +5,12 @@ import org.nustaq.kontraktor.Promise;
 import org.nustaq.offheap.FSTAsciiStringOffheapMap;
 import org.nustaq.offheap.FSTUTFStringOffheapMap;
 
+import java.util.function.Function;
+
 /**
  * Created by ruedi on 26.06.17.
+ *
+ * Kep async as in could be implemented by a database or a (scalable,remote) noSQL e.g. RealLive storage
  */
 public class DefaultSessionStorage implements ISessionStorage {
 
@@ -19,12 +23,29 @@ public class DefaultSessionStorage implements ISessionStorage {
     }
 
     @Override
+    public IPromise atomic(String userId, Function<Object, AtomicResult> recordConsumer) {
+        AtomicResult res = recordConsumer.apply(userData.get(userId));
+        if ( res.getAction() == Action.PUT ) {
+            userData.put(userId,res.getRecord());
+        } else if ( res.getAction() == Action.DELETE ) {
+            userData.remove(userId);
+        }
+        return new Promise(res.getReturnValue());
+    }
+
+    @Override
     public void storeUserRecord(String userId, Object userRecord) {
         userData.put(userId,userRecord);
+    }
+
+    @Override
+    public IPromise storeIfNotPresent(String userId, Object userRecord) {
+        return null;
     }
 
     @Override
     public IPromise getUserRecord(String userId) {
         return new Promise(userData.get(userId));
     }
+
 }
