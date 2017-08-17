1453b52adf70d57f2bd42067d88f59646d6ef85d
RuedigerMoeller <moru0011@gmail.com>
2017-08-17 17:26:35 +0200

    .

---
+++ examples/nodejs-java/node2java/src/main/javascript/node_modules/js4k/stupid_redundancy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cp js4k.js /home/ruedi/projects/kontraktor-spa-example/weblib/js4k/js4k.js
+cp js4k.js /home/ruedi/projects/polystrene/web/js4k/js4k.js
+cp js4k.js /home/ruedi/projects/Juptr/src/main/web/comps/js4k/js4k.js
+cp js4k.js /home/ruedi/projects/polymer-starter-with-kontraktor/src/main/web/lib/js4k/js4k.js
