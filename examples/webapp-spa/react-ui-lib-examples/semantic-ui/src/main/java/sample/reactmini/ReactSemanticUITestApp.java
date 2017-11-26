a78a78de5dd54925dc2df4192c426a7b0fdc327d
RuedigerMoeller <moru0011@gmail.com>
2017-11-26 02:47:35 +0100

    jsx fix k-http 4.18.3

--- examples/webapp-spa/react-ui-lib-examples/semantic-ui/src/main/java/sample/reactmini/ReactSemanticUITestApp.java
+++ examples/webapp-spa/react-ui-lib-examples/semantic-ui/src/main/java/sample/reactmini/ReactSemanticUITestApp.java
@@ -45,7 +45,7 @@ public class ReactSemanticUITestApp extends Actor<ReactSemanticUITestApp> {
     }
 
     public static void main(String[] args) {
-        boolean DEVMODE = true;
+        boolean DEVMODE = false;
 
         if ( ! new File("./src/main/web/client/index.html").exists() ) {
             System.out.println("Please run with working dir: '[..]/semantic-ui");
