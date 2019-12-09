c5c4ed6bb3bc229c9646b49b3c0a3d52587ffb10
RuedigerMoeller <moru0011@gmail.com>
2019-12-09 19:22:38 +0100

    dart play

--- modules/kontraktor-http/src/main/dart/lib/kontraktor-server-playground.dart
+++ modules/kontraktor-http/src/main/dart/lib/kontraktor-server-playground.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:uuid/uuid.dart';
 
 class ReflectionReplacementTest {
 
@@ -8,7 +9,7 @@ class ReflectionReplacementTest {
 
   ReflectionReplacementTest() {
     public = {
-      "login": (String a, String b) async => login( a, b),
+      "login": (String a, String b) => login( a, b),
     };
   }
   Future login( String a, String b) async {
@@ -16,6 +17,7 @@ class ReflectionReplacementTest {
   }
 }
 
+var uuid = Uuid();
 
 main() {
   HttpServer
@@ -24,12 +26,21 @@ main() {
     server.listen((HttpRequest request) async {
       if ( request.method == "POST" ) {
         String postBody = await request.cast<List<int>>().transform(Utf8Decoder(allowMalformed:true)).join();
-        print( postBody );
-        if ( postBody.length > 0 )
-          print( jsonDecode(postBody) );
+        print( "BODY:" + postBody );
+        if ( postBody.length > 0 ) {
+          print("sid:" + request.headers.value("sid"));
+          print(jsonDecode(postBody));
+        }
+        else {
+          // create session id
+          // TODO: generate connection object
+          request.response.write('"'+uuid.v4()+'"');
+          request.response.close();
+        }
+      } else {
+        request.response.write('Hello, world!');
+        request.response.close();
       }
-      request.response.write('Hello, world!');
-      request.response.close();
     });
   });
   print ("done");
