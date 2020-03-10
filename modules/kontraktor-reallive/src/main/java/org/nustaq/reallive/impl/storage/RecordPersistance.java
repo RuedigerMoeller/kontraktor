4eb3dc3a3feeec5eea7055274ca94e17cc7ccc94
RuedigerMoeller <moru0011@gmail.com>
2020-03-10 17:54:32 +0100

    extract Record Json transformator, prepare external persistence

--- modules/kontraktor-reallive/src/main/java/org/nustaq/reallive/impl/storage/RecordPersistance.java
+++ modules/kontraktor-reallive/src/main/java/org/nustaq/reallive/impl/storage/RecordPersistance.java
@@ -3,6 +3,9 @@ package org.nustaq.reallive.impl.storage;
 import org.nustaq.kontraktor.Spore;
 import org.nustaq.reallive.api.Record;
 
+/**
+ * interface can be used in conjunction with CachedRecordStorage to provide a persistance layer
+ */
 public interface RecordPersistance {
 
     Record remove(String key);
@@ -10,7 +13,7 @@ public interface RecordPersistance {
     StorageStats getStats();
 
     default void resizeIfLoadFactorLarger(double loadFactor, long maxGrowBytes) {
-        // do nothing
+        // do nothing, special for built in persistence layer
     }
 
     <T> void forEachWithSpore(Spore<Record, T> spore);
@@ -20,6 +23,5 @@ public interface RecordPersistance {
     default RecordPersistance _put(String key, Record value) {
         throw new RuntimeException("not usable as persistance implementation");
     }
-
 
 }
