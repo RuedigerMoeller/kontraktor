98ad0fad7cace69864d0499363fc2d998203183d
RuedigerMoeller <moru0011@gmail.com>
2020-05-15 19:27:07 +0200

    try with mongodb as persistance engine, turns out "async driver" is sync inside. abysmal throughput

--- modules/service-suppport/src/test/java/rljson/TestRLJson.java
+++ modules/service-suppport/src/test/java/rljson/TestRLJson.java
@@ -10,12 +10,16 @@ import org.nustaq.kontraktor.services.rlserver.RLJsonServer;
 import org.nustaq.kontraktor.services.rlserver.RLJsonSession;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.nustaq.kontraktor.webapp.KJson.*;
 
 public class TestRLJson {
 
     public static void main(String[] args) {
+        boolean QUERY = false;
+        boolean UPSERT = true;
+
         RLJsonServer server = (RLJsonServer) new WebSocketConnectable(RLJsonServer.class, "ws://localhost:8087/ws")
             .coding( new Coding(SerializerType.JsonNoRef,RLJsonServer.CLAZZES) )
             .connect()
@@ -36,23 +40,38 @@ public class TestRLJson {
             ).toString()
         );
 
-        for ( int i = 0; i < 100000; i++ ) {
-            session.update("feed", obj(
-                "key", "5ebd6f0a1637ffa8fa9cc5ef"+i,
-                "array", arr(1,2,3,4,5),
-                "subob", obj("x", i, "random", Math.random() )
-            ).toString());
-            if ( i%1000 == 0 && false ) { // mongodb slowdown
-                try {
-                    System.out.println("sleep "+i);
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        long tim = System.currentTimeMillis();
+
+        if ( UPSERT ) {
+            for (int i = 0; i < 30_000; i++) {
+                session.update("feed", obj(
+                    "key", "5ebd6f0a1637ffa8fa9cc5ef" + i,
+                    "array", arr(1, 2, 3, 4, 5),
+                    "subob", obj("x", i, "random", Math.random())
+                ).toString());
+                if (i % 1000 == 0 && false) { // mongodb slowdown
+                    try {
+                        System.out.println("sleep " + i);
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
 
-        Object await1 = session.get("feed", "POKPOKPOK").await();
+        if ( QUERY ) {
+            AtomicInteger count = new AtomicInteger();
+            session.select("feed", "true", (r,er) -> {
+                if ( r != null )
+                    count.incrementAndGet();
+                else
+                    System.out.println("query duration "+(System.currentTimeMillis()-tim)+" count "+count);
+            });
+        }
+
+        Object await1 = session.get("feed", "POKPOKPOK").await(500_000);
+        System.out.println("duration "+(System.currentTimeMillis()-tim));
         System.out.println(await1);
     }
 }
