4ad589764c4edf3de8486dc3e111006f5dc6ce9b
RuedigerMoeller <moru0011@gmail.com>
2020-06-02 15:08:23 +0200

    build woes

--- release.sh
+++ release.sh
@@ -1,71 +1,65 @@
 #!/usr/bin/env bash
 
+# MAC export GPG_TTY=$(tty)
+
 export PW=$1
 
 echo $1
 
-mvn clean package -Dmaven.test.skip=true gpg:sign -Dgpg.passphrase=$PW
+mvn clean package -Dmaven.test.skip=true -DadditionalJOption=-Xdoclint:none gpg:sign -Dgpg.passphrase=$PW
 mkdir target
 cd target
 rm *-jar-with*
 rm -r */
 jar -cf bundle.jar *
 cd ..
-mvn package install -Dmaven.test.skip=true # rebuild fat jars
+mvn package install -Dmaven.test.skip=true -DadditionalJOption=-Xdoclint:none  # rebuild fat jars
 
-cd modules/kontraktor-bare
-
-mvn clean package -Dmaven.test.skip=true gpg:sign  -Dgpg.passphrase=$PW
-mkdir target
-cd target
-rm -r */
-rm *-jar-with-dep*
-jar -cf bundle.jar *
-cd ..
-mvn package -Dmaven.test.skip=true # rebuild fat jars
+#cd modules/kontraktor-bare
+#
+#mvn clean package -Dmaven.test.skip=true gpg:sign  -Dgpg.passphrase=$PW
+#mkdir target
+#cd target
+#rm -r */
+#rm *-jar-with-dep*
+#jar -cf bundle.jar *
+#cd ..
+#mvn package -Dmaven.test.skip=true # rebuild fat jars
 
 cd ../kontraktor-http
 
-mvn clean package -Dmaven.test.skip=true gpg:sign  -Dgpg.passphrase=$PW
+mvn clean package -Dmaven.test.skip=true -DadditionalJOption=-Xdoclint:none gpg:sign  -Dgpg.passphrase=$PW
 mkdir target
 cd target
 rm -r */
 rm khttp.jar*
 jar -cf bundle.jar *
 cd ..
-mvn install -Dmaven.test.skip=true # install
+mvn install -Dmaven.test.skip=true -DadditionalJOption=-Xdoclint:none # install
 
-cd ../reactive-streams
-
-mvn clean package -Dmaven.test.skip=true gpg:sign  -Dgpg.passphrase=$PW
-mkdir target
-cd target
-rm *-jar-with*
-rm -r */
-jar -cf bundle.jar *
-cd ..
+#cd ../reactive-streams
+#
+#mvn clean package -Dmaven.test.skip=true -DadditionalJOption=-Xdoclint:none gpg:sign  -Dgpg.passphrase=$PW
+#mkdir target
+#cd target
+#rm *-jar-with*
+#rm -r */
+#jar -cf bundle.jar *
+#cd ..
+export GPG_TTY=$(tty)
 
 cd ../kontraktor-reallive
-mvn clean package -Dmaven.test.skip=true gpg:sign  -Dgpg.passphrase=$PW
+mvn clean package -Dmaven.test.skip=true -DadditionalJOption=-Xdoclint:none gpg:sign  -Dgpg.passphrase=$PW
 mkdir target
 cd target
 rm *-jar-with*
 rm -r */
 jar -cf bundle.jar *
 cd ..
-mvn package install -Dmaven.test.skip=true # rebuild fat jars
-
-cd ../kontraktor-web
-mvn clean package -Dmaven.test.skip=true gpg:sign  -Dgpg.passphrase=$PW
-mkdir target
-cd target
-rm *-jar-with*
-rm -r */
-jar -cf bundle.jar *
-cd ..
+mvn package install -Dmaven.test.skip=true -DadditionalJOption=-Xdoclint:none # rebuild fat jars
 
 cd ../service-suppport
-mvn clean package -Dmaven.test.skip=true gpg:sign  -Dgpg.passphrase=$PW
+mvn clean package -Dmaven.test.skip=true -DadditionalJOption=-Xdoclint:none gpg:sign -Dgpg.passphrase=$PW
 mkdir target
 cd target
 rm *-jar-with*
