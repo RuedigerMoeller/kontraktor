1e1cb84921fd620eb56d273d4e246e3ee1bfe2fa
Mario Neises <git@mnio.net>
2023-07-04 11:20:34 +0200

    add example documentation

--- modules/kontraktor-reallive/src/main/java/org/nustaq/reallive/api/LimitedQuery.java
+++ modules/kontraktor-reallive/src/main/java/org/nustaq/reallive/api/LimitedQuery.java
@@ -5,6 +5,14 @@ import java.io.Serializable;
 /**
  * limits the amount of tested records per datanode. This can be useful to avoid transmission of all matching
  * records in find-alike scenarios.
+ *
+ * Query will be executed on each data node.
+ * Therefore, max. result size will be `limit * amount` of data nodes
+ * Example:
+ * limit 5, node count: 2, max result size can be 10 items
+ * - dataNode1 returns 5 items (limit is fulfilled)
+ * - dataNode2 returns 3 items (more does not match sub predicate)
+ * result: 8 items
  */
 public class LimitedQuery implements Serializable, RLPredicate<Record> {
 
